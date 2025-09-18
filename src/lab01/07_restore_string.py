cd3261a5762407a2c36955cee5ac1aa61e78dd64
skranz-kul <m2502939@edu.misis.ru>
2025-09-18 11:11:15 +0300

    Update 07_restore_string.py

--- src/lab01/07_restore_string.py
+++ src/lab01/07_restore_string.py
@@ -1,53 +1,21 @@
-s = input().strip()
+interminal = input('in: ')
+slovo = ''
+index1 = 0 
+index2 = 0
 
-letters = [(i, ch) for i, ch in enumerate(s) if ch.isalpha()]
-
-p0 = None
-for pos, (idx, ch) in enumerate(letters):
-    if ch.isupper():
-        p0 = pos
+for i in range(len(interminal)):
+    if interminal[i].isupper():
+        index1 = i 
         break
-
-result = ""
-if p0 is not None:
-    n_letters = len(letters)
-    n = len(s)
-
-    candidate_p1 = [pos for pos, (idx, _ch) in enumerate(letters) if idx - 1 >= 0 and s[idx - 1].isdigit()]
-
-    found = False
-    for p1 in candidate_p1:
-        k = p1 - p0
-        if k == 0:
-            continue
-        built_letters = []
-        p = p0
-        while 0 <= p < n_letters:
-            idx, ch = letters[p]
-            built_letters.append(ch)
-            if idx + 1 < n and s[idx + 1] == ".":
-                result = ("".join(built_letters)).lower() + "."
-                found = True
-                break
-            p += k
-
-        if found:
-            break
-    if not found:
-        for p1 in candidate_p1:
-            k = p1 - p0
-            if k == 0:
-                continue
-            built_letters = []
-            p = p0
-            while 0 <= p < n_letters:
-                idx, ch = letters[p]
-                built_letters.append(ch)
-                p += k
-            if built_letters:
-                result = ("".join(built_letters)).lower()
-                break
-
-print(result)
-
-
+    else:
+        continue 
+for i in range(len(interminal)):
+    if interminal[i] in '0123456789':
+        index2 = i+1
+        break
+    else:
+        continue
+shag = index2 - index1
+for i in range(index1,len(interminal),shag):
+    slovo+=interminal[i]
+print(f'out: {slovo}')
