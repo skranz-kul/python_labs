bea2ccac650d0b1f28947233c1f3d0987d1af1de
Ярослав <m2502939@edu.misis.ru>
2025-10-29 19:32:52 +0300

    tests ready

---
+++ src/lab05/csv_xlsx.py
@@ -0,0 +1,29 @@
+from openpyxl import Workbook
+import csv
+
+def csv_to_xlsx(csv_path: str, xlsx_path: str) -> None:
+    """
+    Конвертирует CSV в XLSX.
+    Использовать openpyxl ИЛИ xlsxwriter.
+    Первая строка CSV — заголовок.
+    Лист называется "Sheet1".
+    Колонки — автоширина по длине текста (не менее 8 символов).
+    """
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Sheet1"
+    
+    with open(csv_path, encoding="utf-8") as f:
+        for row in csv.reader(f):
+                ws.append(row)
+        for column in ws.columns:
+            mx = 0
+            column_letter = column[0].column_letter
+            for cell in column:
+                mx = max(mx, len(cell.value))
+            new_width = max(mx + 2, 8)
+            print(ws.column_dimensions)
+            ws.column_dimensions[column_letter].width = new_width
+    
+    wb.save(xlsx_path)
+csv_to_xlsx('src/lab05/json_to_csv_converted_file.csv', 'src/lab05/csv_to_xlsx.xlsx')
--- src/lab05/json_csv.py
+++ src/lab05/json_csv.py
@@ -1,5 +1,6 @@
 import json
 import csv
+from queue import Empty
 
 def json_to_csv(json_path: str, csv_path: str) -> None:
     """
@@ -11,13 +12,15 @@ def json_to_csv(json_path: str, csv_path: str) -> None:
         raise TypeError
     with open(json_path, encoding="utf-8") as f: 
         data = json.load(f)
+    if not data or type(data[0]) != dict:
+        raise ValueError("Пустой JSON или неподдерживаемая структура")
     with open(csv_path, "w", newline="", encoding="utf-8") as cf:
         writer = csv.DictWriter(cf, fieldnames=list(data[0].keys()))
         writer.writeheader()
         writer.writerows(data)
 
 
-json_to_csv('data/samples/people.json', 'src/lab05/json_to_csv_converted_file.csv')
+json_to_csv('data/samples/test.json', 'src/lab05/json_to_csv_converted_file.csv')
 
 
 def csv_to_json(csv_path: str, json_path: str) -> None:
@@ -33,9 +36,10 @@ def csv_to_json(csv_path: str, json_path: str) -> None:
         reader = csv.DictReader(cf)
         for row in reader:
             lt_rows.append(row)
-        print(lt_rows)
+        if not lt_rows:
+            raise ValueError
     with open(json_path, 'w', encoding='utf-8') as jf:
         json.dump(lt_rows, jf, ensure_ascii=False, indent=2)
 
 
-csv_to_json('data/samples/cities.csv', 'src/lab05/csv_to_json_example.json')
+csv_to_json('data/samples/test.csv', 'src/lab05/csv_to_json_example.json')
